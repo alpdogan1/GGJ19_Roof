a9fadaefad5c79abce8e7c89da19f6f366ac39df
Doguscan Ozturk <doguscanozturk@gmail.com>
2019-01-27 02:41:12 +0300

    Replacing camera function added to the CameraFollow script.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,48 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : Singleton<CameraFollow> {
+public class CameraFollow : Singleton<CameraFollow>
+{
 
-	public Camera mainCamera;
-	public Transform cameraTransform;
-	public GameObject cameraObject;
-	public Transform followTarget;
-	public Vector3 followOffset;
+    public Camera mainCamera;
+    public Transform cameraTransform;
+    public GameObject cameraObject;
+    public Transform followTarget;
+    public Vector3 followOffset;
 
-	private bool isFollowing;
+    private Vector3 TargetPosition
+    {
+        get
+        {
+            if (followTarget != null)
+                return followTarget.position + followOffset;
+            else
+                return Vector3.zero;
+        }
+    }
+    private bool isFollowing;
+    private bool isReplacing;
 
-	void LateUpdate()
-	{
-		if (isFollowing) {
-			if (followTarget != null) {
-			
-				cameraTransform.position = followTarget.position + followOffset; 
-			
-			}
-		
-		}
+    [SerializeField]
+    private Vector3 defaultRotation;
+    [SerializeField]
+    private float defaultFoV;
 
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ReplaceCamera(TargetPosition, defaultRotation, defaultFoV, 1f, true);
 
-	public void TriggerFollow(bool isFollowing, Transform target=null)
-	{
-		this.isFollowing = isFollowing;
+        }
 
-		if(target != null)
-		{
-			followTarget = target;
-		}
 
-	}
-	public void ReplaceCamera(Vector3 position, Vector3 localEulerRotation, float fieldOfView, float duration)
-	{
-			TriggerFollow (false);
+    }
+    void LateUpdate()
+    {
+        if (isFollowing)
+        {
+            if (followTarget != null)
+            {
 
-		LeanTween.move (cameraObject, position, duration).setEase (easeType: LeanTweenType.easeInOutQuart);
-		LeanTween.rotateLocal (cameraObject, localEulerRotation, duration).setEase (easeType: LeanTweenType.easeOutQuart);
-		LeanTween.value (mainCamera.fieldOfView, fieldOfView, duration).setOnUpdate((float val)=>{
-			mainCamera.fieldOfView = val;
-		});
-	}
+                cameraTransform.position = followTarget.position + followOffset;
+
+            }
+        }
+
+    }
+
+    public void TriggerFollow(bool isFollowing, Transform target = null)
+    {
+        this.isFollowing = isFollowing;
+
+        if (target != null)
+        {
+            followTarget = target;
+        }
+
+    }
+    public void ReplaceCamera(Vector3 position, Vector3 localEulerRotation, float fieldOfView, float duration, bool startFollowingAfterReplaced = false)
+    {
+        if (!isReplacing)
+        {
+            isReplacing = true;
+            TriggerFollow(false);
+
+            LeanTween.move(cameraObject, position, duration).setEase(easeType: LeanTweenType.easeInOutQuart);
+            LeanTween.rotateLocal(cameraObject, localEulerRotation, duration * 2).setEase(easeType: LeanTweenType.easeOutQuart);
+            LeanTween.value(mainCamera.fieldOfView, fieldOfView, duration).setOnUpdate((float val) =>
+            {
+                mainCamera.fieldOfView = val;
+            }).setOnComplete(OnReplaced());
+        }
+    }
+    private void OnReplaced(bool startFollowingAfterReplaced)
+    {
+        isReplacing = false;
+        if (startFollowingAfterReplaced)
+        {
+            TriggerFollow(true, followTarget);
+        }
+    }
 
 }
--- Assets/Scripts/MainVariables.cs
+++ Assets/Scripts/MainVariables.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class MainVariables : Singleton<MainVariables>
 {
     public float GridWidth = 1f;
-    public float PlayerRotationDuration = 1f;
+    public float PlayerRotationDuration = 0.3f;
     public LeanTweenType PlayerRotationEasing = LeanTweenType.easeOutQuad;
 }
