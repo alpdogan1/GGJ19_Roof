dd6546e31b9ebc4168ba9cd8b2ab2e6744450217
Chad <yildirimcagdas1@gmail.com>
2019-01-27 10:46:01 +0300

    New level setup

--- Assets/Scripts/MainVariables.cs
+++ Assets/Scripts/MainVariables.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class MainVariables : Singleton<MainVariables>
 {
     public float GridWidth = 1f;
-    public float PlayerRotationDuration = 1f;
-    public LeanTweenType PlayerRotationEasing = LeanTweenType.easeOutQuad;
+    public float PlayerRotationDuration = .3f;
+    public LeanTweenType PlayerRotationEasing = LeanTweenType.easeInQuart;
 }
