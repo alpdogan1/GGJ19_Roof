459be5e82ea77f84d1cefc06559239a59cea1382
Alp Dogan Urut <alpdoganurut@gmail.com>
2019-01-28 14:13:38 +0300

    MasterAudio and stuff. Intro scene.

---
+++ Assets/AnyKeySceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AnyKeySceneLoader : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("main");
+        }
+    }
+}
